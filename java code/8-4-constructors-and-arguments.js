19d54b69bab12640f9edf9644bcfe7e62b31e616
sxena <s_xena@hotmail.com>
2020-06-02 23:03:19 -0700

    arrays and bubbles with the  array

--- java code/8-4-constructors-and-arguments.js
+++ java code/8-4-constructors-and-arguments.js
@@ -1,3 +1,77 @@
+// *************************************************
+// *************************************************
+// *************************************************
+// floating bubbles
+// *************************************************
+// *************************************************
+// *************************************************
+
+Bubble b1;
+Bubble b2;
+
+
+void settings(){
+  size (600, 600);
+  b1 = new Bubble(64);
+  b2 = new Bubble(16);
+}
+
+void draw(){
+  background(225);
+  b1.display();
+  b1.assending();;
+  b1.top();
+
+  b2.display();
+  b2.assending();;
+  b2.top();
+
+  //b1.x = mouseX;
+  //b1.y = mouseY;
+
+}
+
+class Bubble {
+  float x;
+  float y;
+  float diameter;
+  //color bubbleColor;
+
+  Bubble(float tempD) {
+    x = width/2;
+    y = height;
+    diameter = tempD;
+    //bubbleColor = tempC;
+  }
+  void assending(){
+    --y;
+    x = x + random (-2,2);
+  }
+  void top(){
+    if (y < diameter/2){
+      y = diameter/2;
+    }
+  }
+  void display(){
+    stroke(0);
+    fill(127);
+    ellipse(x, y, diameter, diameter);
+  }
+
+}
+
+
+
+
+
+
+// *************************************************
+// *************************************************
+// *************************************************
+// added color
+// *************************************************
+// *************************************************
+// *************************************************
 Bubble b1;
 Bubble b2;
 
@@ -18,8 +92,8 @@ void draw(){
   b2.assending();;
   b2.top();
 
-  b1.x = mouseX;
-  b1.y = mouseY;
+  // b1.x = mouseX;
+  // b1.y = mouseY;
 
 }
 
--- java code/8-5-create-the-overlaping.js
+++ java code/8-5-create-the-overlaping.js
@@ -11,9 +11,11 @@ void settings(){
 
 void draw(){
   background(115);
+
   if(p1.overlaps(p2)){
     background(230,11,0);
   }
+
   p1.display();
   p2.display();
   p3.display();
@@ -21,6 +23,7 @@ void draw(){
   p2.x = mouseX;
   p2.y = mouseY;
 }
+
 class Particle {
   float x, y;
   float r;
@@ -47,6 +50,75 @@ class Particle {
     stroke(255);
     strokeWeight(4);
     noFill();
+    ellipse(x,y,r*2,r*2);
+  }
+}
+
+// *************************************************
+// *************************************************
+// *************************************************
+// pass by refrence
+// *************************************************
+// *************************************************
+// *************************************************
+
+Particle p1;
+Particle p2;
+Particle p3;
+
+void settings(){
+  size(600,600);
+  p1 = new Particle (100,100,50);
+  p2 = new Particle (100,150,60);
+  p3 = new Particle();
+}
+
+void draw(){
+  background(115);
+
+  p1.overlaps(p2); // changed not into a function but a call
+  p3.overlaps(p2); //
+
+  p1.display();
+  p2.display();
+  p3.display();
+
+  p2.x = mouseX;
+  p2.y = mouseY;
+}
+
+class Particle {
+  float x, y;
+  float r;
+  color col; // added new local variable
+
+  Particle(){
+    x = random(width);
+    y = random(height);
+    r = random(4,18);
+    col = color(0); // added the value of color even though if it is not used nothing changes. i am going to go and guesse that this is for the order and organization. man i am freaking out... chill chill chill... i am good thank you
+  }
+  Particle(float tempX, float tempY, float tempR){
+    x = tempX;
+    y = tempY;
+    r = tempR;
+  }
+  void overlaps(Particle nextGen){ //changed from boolean to void
+    float d = dist(x,y,nextGen.x,nextGen.y);
+    if (d < r + nextGen.r){
+      // return true;     // delet that
+      col = color(0,255,0,100);
+      nextGen.col = color(255,0,0,100);
+    }else {
+      // return false;   // delet that
+      col = color(0,100);
+      nextGen.col = color(0,100);
+    }
+  }
+  void display(){
+    stroke(255);
+    strokeWeight(4);
+    fill(col); // changed from noFill() to fill(col)
     ellipse(x,y,r*2,r*2);
   }
 }
